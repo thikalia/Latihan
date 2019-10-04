bd45d3ec9e557ef47ea31cb144a2c0cb81d2d441
thikalia <kaathik26@gmail.com>
2019-10-04 09:46:39 +0700

    refactor

--- sarpras/sarpras/library/connect.php
+++
@@ -1,8 +0,0 @@
-<?
-$host="localhost";
-$user="root";
-$pas="";
-$database="dbinventori";
-mysql_connect($host,$user,$pas)or die("Tidak connect");
-mysql_select_db($database)or die("Data base tidak ada");
-?>
